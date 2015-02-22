e095e52b10f74c635164f338d7249309301d5fc8
jonaoost@gmail.com <jonaoost@gmail.com@311ddcac-758c-2446-7104-03476af7627b>
2015-02-22 15:52:37 +0000

    Wiped entire repo

--- trunk/ogp-jumping-alien/src-provided/jumpingalien/common/game/JumpingAlienGameOptions.java
+++
@@ -1,9 +0,0 @@
-package jumpingalien.common.game;
-
-import ogp.framework.game.GameOptions;
-
-public interface JumpingAlienGameOptions extends GameOptions {
-
-	public double getTimescale();
-	public void setTimescale(double value);
-}
