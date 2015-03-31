99be6268c3e277261033d9d980db1282ce2edf83
andreas_schryvers <andreas_schryvers@hotmail.com>
2015-03-31 09:09:30 +0000

    meerdere lege klassen toegevoegd

---
+++ OGP1415-JumpingAlien/src/jumpingalien/model/Slime.java
@@ -0,0 +1,22 @@
+package jumpingalien.model;
+
+public class Slime extends GameObject {
+
+	public Slime(World world, double x, double y) {
+		super(world, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
