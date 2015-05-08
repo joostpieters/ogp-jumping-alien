cfcc541189116d843b1608a20f778cfcd1bd6c1a
jonaoost <jonaoost@gmail.com>
2015-05-08 10:36:50 +0000

    Program attributes

--- OGP1415-JumpingAlien/src/jumpingalien/model/Program.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/Program.java
@@ -7,24 +7,19 @@ import be.kuleuven.cs.som.annotate.*;
 
 public class Program {
 	
-	private Map<String, GameObject> objMap;
-	private Map<String, Direction> dirMap;
-	private Map<String, Double>	doubleMap;
-	private Map<String, Boolean> boolMap;
+	
 
 	public Program() {
 		GAME_OBJECT = null;
+		MAIN_STATEMENT = null;
 		objMap = new HashMap<String, GameObject>();
 		dirMap = new HashMap<String, Direction>();
 		doubleMap = new HashMap<String, Double>();
 		boolMap = new HashMap<String, Boolean>();
+		setTimeRemaining(0);
+		setWaitTime(0);
 	}
-	
-	private GameObject GAME_OBJECT;
-	private Statement MAIN_STATEMENT;
-	
-	
-	
+
 	public GameObject getGameObject() {
 		return GAME_OBJECT;
 	}
@@ -41,7 +36,71 @@ public class Program {
 	public void setMainStatement(Statement statement) {
 		assert MAIN_STATEMENT == null;
 		MAIN_STATEMENT = statement;
+		setCurrentStatement(statement);
 	}
+	
+	private GameObject GAME_OBJECT;
+	private Statement MAIN_STATEMENT;
+	
+	private Map<String, GameObject> objMap;
+	private Map<String, Direction> dirMap;
+	private Map<String, Double>	doubleMap;
+	private Map<String, Boolean> boolMap;
+	
+	public Statement getCurrentStatement() {
+		return currentStatement;
+	}
+	
+	public void setCurrentStatement(Statement statement) {
+		this.currentStatement = statement;
+	}
+	
+	private Statement currentStatement;
+	
+	@Basic
+	public double getWaitTime() {
+		return waitTime;
+	}
+	
+	public void addWaitTime(double time) {
+		setWaitTime(getWaitTime() + time);
+	}
+	
+	public void substractWaitTime(double time) {
+		setWaitTime(getWaitTime() - time);
+	}
+	
+	private void setWaitTime(double time) {
+		waitTime = time;
+		if(time < 0) {
+			waitTime = 0;
+		}
+	}
+	
+	private double waitTime;
+	
+	@Basic
+	public double getTimeRemaining() {
+		return timeRemaining;
+	}
+	
+	public void addTimeRemaining(double time) {
+		setTimeRemaining(getTimeRemaining() + time);
+	}
+	
+	public void substractTimeRemaining(double time) {
+		setTimeRemaining(getTimeRemaining() - time);
+	}
+	
+	private void setTimeRemaining(double time) {
+		timeRemaining = time;
+		if(time < 0) {
+			timeRemaining = 0;
+		}
+	}
+	
+	private double timeRemaining;
+	
 	
 	public void initialiseVariables(Map<String, Type> globalVariables) {
 		for (String key : globalVariables.keySet()) {
